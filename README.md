56342265002e313699d095db95bf3bce296f66eb
iamHrithikRaj <iamHrithikRaj26@gmail.com>
2022-03-10 13:55:04 +0530

    added README

---
+++ README.md
@@ -0,0 +1,41 @@
+# CyberHack Backend API
+
+### Team Login
+
+```bash
+curl -X POST https://pure-brook-94362.herokuapp.com/api/v1/team/login
+```
+
+```json
+{
+    "teamName": "CyberPunk1",
+    "password": "1234567"
+}
+```
+
+
+
+
+
+###  Increment Team's Score
+
+```bash
+curl -X POST https://pure-brook-94362.herokuapp.com/api/v1/team/submit
+```
+
+```json
+{
+    "points": 110
+}
+```
+
+
+
+
+
+### Get Team's Score
+
+```bash
+curl -X GET https://pure-brook-94362.herokuapp.com/api/v1/team/rank
+```
+
--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 // Import packages
 const express = require("express");
 const app = express();
-const dotenv = require("dotenv")
+const dotenv = require("dotenv");
 const mongoose = require("mongoose");
-const cors = require('cors')
-
+const cors = require("cors");
+const swaggerUi = require("swaggerUi");
 
 //Import Routes
 const register = require("./routes/register");
@@ -12,26 +12,39 @@ const login = require("./routes/login");
 const submit = require("./routes/Submit");
 const rank = require("./routes/Rank");
 
-
-dotenv.config({path : "./.env"});
+dotenv.config({ path: "./.env" });
 
 // Connect to DB
 mongoose.connect(
-  "mongodb+srv://cyberhack-db:cyberhackamity2022@cluster0.wo9hq.mongodb.net/CyberHack?retryWrites=true&w=majority", { useNewUrlParser: true });
+  "mongodb+srv://cyberhack-db:cyberhackamity2022@cluster0.wo9hq.mongodb.net/CyberHack?retryWrites=true&w=majority",
+  { useNewUrlParser: true }
+);
 
 // CORS Configuration
 const corsOptions = {
-  origin: '*',
-  optionsSuccessStatus: 200 
-}
+  origin: "*",
+  optionsSuccessStatus: 200,
+};
 
-//Middlewares 
-app.use(express.json())
-app.use(cors(corsOptions))
+//Middlewares
+app.use(express.json());
+app.use(cors(corsOptions));
 
-
-//Routes Middleware 
+//Routes Middleware
 app.use("/api/v1/team", [register, login, submit, rank]);
 
+// OpenAPI UI
+app.use(
+  "/api-documentation",
+  swaggerUi.serve,
+  swaggerUi.setup(null, {
+    swaggerOptions: {
+      url: "http://localhost:3030/api-docs",
+    },
+  })
+);
+
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, () => console.log(`Server running at http://127.0.0.1:${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Server running at http://127.0.0.1:${PORT}`)
+);
